ec5d7a56184d7418968de4ea8cbc2a9fb10495c3
Matheus Damião <matheus.damiaoliveira@gmail.com>
2022-09-26 17:53:12 -0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ function IndexPage({ data: { allGraphCmsPost } }) {
           Latest
         </h1>
         <p className="text-lg leading-7 text-gray-500">
-          Our latest blog posts.
+          últimas postagens
         </p>
       </div>
 
